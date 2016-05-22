be3944920b63ac4966332d8e1ce96422578e4e98
rafareypy <rreynoud@gmail.com>
2016-05-22 13:57:51 -0300

    Ajustando obter foto

--- src/br/com/pactus/mobile/manager/CandidatosManager.java
+++ src/br/com/pactus/mobile/manager/CandidatosManager.java
@@ -1,5 +1,7 @@
 package br.com.pactus.mobile.manager;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,11 +10,16 @@ import javax.commons.core.tool.Console;
 import javax.commons.core.util.NumUtils;
 import javax.commons.core.util.ObjUtils;
 import javax.commons.persistence.PersistenceClause;
+import javax.faces.FacesException;
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.imageio.stream.FileImageOutputStream;
+
+import org.primefaces.event.CaptureEvent;
 
 import br.com.mediastinum.pectus.business.common.session.facade.SessionManager;
 import br.com.mediastinum.pectus.business.model.entity.Parceiro;
@@ -22,24 +29,26 @@ import br.com.mediastinum.pectus.business.model.entity.Parceiro;
 public class CandidatosManager implements Serializable{
 
    private static final long serialVersionUID = 1L;
-
+   private String filename;
    public Parceiro parceiroEdicao = new Parceiro();
    public List<Parceiro> parceiros = new ArrayList<Parceiro>();
 
    public CandidatosManager(){
 
+      parceiroEdicao.setParAltura(50d);
+      
       PersistenceClause clause = new PersistenceClause();
       clause.andEquals("camId", 24);
       Parceiro p = new Parceiro();
       p.addClause(clause);
       List<Parceiro> parceirosAUX = SessionManager.search().findAllByClause(p).getResult();
-      
-      int i =0 ;
+
+      int i = 0;
       for (Parceiro parceiro: parceirosAUX){
          i++;
-         if(NumUtils.isLessThan(i, 4)){
+         if (NumUtils.isLessThan(i, 4)){
             parceiros.add(parceiro);
-         }else{
+         } else{
             return;
          }
       }
@@ -62,14 +71,14 @@ public class CandidatosManager implements Serializable{
    public String buscarParceiro(){
       Long codigo = parceiroEdicao.getParId();
       Parceiro p = new Parceiro();
-      p.setParId(codigo);      
+      p.setParId(codigo);
       parceiroEdicao = SessionManager.search().findById(p).getResult();
-      
-      if(ObjUtils.isNull(parceiroEdicao)){
+
+      if (ObjUtils.isNull(parceiroEdicao)){
          parceiroEdicao = new Parceiro();
-         this.criaMensagem("Não foram encontrados resultados para o codigo " +codigo+ ".", FacesMessage.SEVERITY_FATAL);
+         this.criaMensagem("Não foram encontrados resultados para o codigo " + codigo + ".", FacesMessage.SEVERITY_FATAL);
       }
-      
+
       return "avaliacao?faces-redirect=true";
    }
 
@@ -112,4 +121,36 @@ public class CandidatosManager implements Serializable{
    public static long getSerialversionuid(){
       return serialVersionUID;
    }
+
+   // ============================================================
+   // Gravação foto
+   // ============================================================
+
+   private String getRandomImageName(){
+      int i = (int) (Math.random() * 10000000);
+
+      return String.valueOf(i);
+   }
+
+   public String getFilename(){
+      return filename;
+   }
+
+   public void oncapture(CaptureEvent captureEvent){
+      filename = getRandomImageName();
+      byte[] data = captureEvent.getData();
+
+      ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+      String newFileName = externalContext.getRealPath("") + File.separator + "resources" + File.separator + "demo" + File.separator + "images" + File.separator + "photocam" + File.separator + filename + ".png";
+
+      FileImageOutputStream imageOutput;
+      try{
+         imageOutput = new FileImageOutputStream(new File(newFileName));
+         imageOutput.write(data, 0, data.length);
+         imageOutput.close();
+      } catch (IOException e){
+         throw new FacesException("Error in writing captured image.", e);
+      }
+   }
+
 }
--- src/br/com/pectus/mobile/login/LoginBean.java
+++ src/br/com/pectus/mobile/login/LoginBean.java
@@ -6,65 +6,51 @@ import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
 import br.com.caelum.livraria.bean.MenuBean;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-import javax.commons.core.common.SystemContext;
-import javax.commons.core.tool.NetInfo;
-import javax.commons.core.tool.SystemInfo;
-import javax.commons.core.util.ObjUtils;
-
-import br.com.mediastinum.pectus.business.common.constant.BusinessMessages;
 import br.com.mediastinum.pectus.business.common.entity.Usuario;
 import br.com.mediastinum.pectus.business.common.session.bean.AccessControlBean;
 import br.com.mediastinum.pectus.business.common.session.facade.SessionManager;
 import br.com.mediastinum.pectus.business.common.session.service.AccessControlService;
-import br.com.mediastinum.pectus.business.model.entity.Parametro;
-
 
 @Model
-public class LoginBean {
-	
-	private Usuario usuarioEncontrado = new Usuario();	
-	
-	@Inject
-	UsuarioLogadoBean usuarioLogado;
-	
-	@Inject
-	MenuBean menu;
+public class LoginBean{
 
-	public Usuario getUsuario() {
-		return usuarioEncontrado;
-	}
-	
-	public String efetuaLogin() {
+   private Usuario usuarioEncontrado = new Usuario();
 
-	   this.usuarioEncontrado = ((AccessControlService) SessionManager.service(AccessControlBean.class)).isValidConnection(usuarioEncontrado);
+   @Inject
+   UsuarioLogadoBean usuarioLogado;
+
+   @Inject
+   MenuBean menu;
+
+   public Usuario getUsuario(){
+      return usuarioEncontrado;
+   }
+
+   public String efetuaLogin(){
+
+      this.usuarioEncontrado = ((AccessControlService) SessionManager.service(AccessControlBean.class)).isValidConnection(usuarioEncontrado);
       if (!usuarioEncontrado.hasErrors()){
-			usuarioLogado.logar(usuarioEncontrado);
-			criaMensagem("Usuario logado!");
-			return menu.paginaAvaliacao();
+         usuarioLogado.logar(usuarioEncontrado);
+         criaMensagem("Usuario logado!");
+         return menu.paginaAvaliacao();
       }
-		criaMensagem("Usuário não encontrado!");
-		limparForm();
-		
-		return "";
-	}
-	
-	public String efetuaLogout() {
-		this.usuarioLogado.deslogar();
-		return this.menu.paginaLogin();
-	}
+      criaMensagem("Usuário não encontrado!");
+      limparForm();
 
-	
-	private void limparForm() {
-		this.usuarioEncontrado = new Usuario();
-	}
+      return "";
+   }
 
-	private void criaMensagem(String mensagem) {
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, ""));
-	}
+   public String efetuaLogout(){
+      this.usuarioLogado.deslogar();
+      return this.menu.paginaLogin();
+   }
 
+   private void limparForm(){
+      this.usuarioEncontrado = new Usuario();
+   }
+
+   private void criaMensagem(String mensagem){
+      FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, ""));
+   }
 
 }
